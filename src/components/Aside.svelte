c3d5ce1a209c3f7ec63e9e2321dbf2f0832b2bf7
drillprops <drylbartosz@gmail.com>
2019-06-05 18:41:57 +0200

    add image to aside

--- src/components/Aside.svelte
+++ src/components/Aside.svelte
@@ -1,1 +1,13 @@
-<aside>this is aside component</aside>
+<style>
+  img {
+    display: block;
+    margin: 0;
+    max-width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+</style>
+
+<aside>
+  <img src="/assets/usd.jpg" alt="USD" />
+</aside>
