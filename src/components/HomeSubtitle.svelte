e196cdb11cf743b264865e8dcf681b2defeae325
drillprops <drylbartosz@gmail.com>
2019-06-08 10:55:37 +0200

    create new component for h3 subtitles, add if statements based on swapped props

---
+++ src/components/HomeSubtitle.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let swapped;
+  export let currency;
+</script>
+
+<style>
+  h3 {
+    margin: 0;
+    margin-bottom: 20px;
+    font-size: 18px;
+    font-weight: 400;
+    color: #555;
+  }
+</style>
+
+{#if swapped}
+  <h3>Convert PLN to {currency}</h3>
+{:else}
+  <h3>Convert {currency} to PLN</h3>
+{/if}
