86cc637d34aaaaa07a0e577e5c97a215c8a0926e
drillprops <drylbartosz@gmail.com>
2019-06-08 10:52:12 +0200

    remove h3 subtittle from component

--- src/components/Homeheader.svelte
+++ src/components/Homeheader.svelte
@@ -10,17 +10,8 @@
     font-size: 40px;
     font-weight: 800;
   }
-  h3 {
-    margin: 0;
-    margin-bottom: 20px;
-    font-size: 18px;
-    font-weight: 400;
-    color: #555;
-  }
 </style>
 
 <header>
   <h1>Currency Converter</h1>
-  <h3>Convert PLN to USD</h3>
-  <h3>Convert USD to PLN</h3>
 </header>
