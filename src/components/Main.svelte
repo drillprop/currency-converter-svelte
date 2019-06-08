b2a72c56954130381b246c7d4510c48f23999398
drillprops <drylbartosz@gmail.com>
2019-06-08 13:15:52 +0200

    move select input to separate component

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,17 +1,9 @@
 <script>
   import HomeHeader from "./HomeHeader.svelte";
   import HomeSubtitle from "./HomeSubtitle.svelte";
+  import Select from "./Select.svelte";
   export let swapped;
   export let pickedCurrency;
-  export let currencyArray;
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
-
-  const dispatchChange = e => {
-    dispatch("changeCurrency", {
-      selected: e.target.value
-    });
-  };
 </script>
 
 <style>
@@ -20,21 +12,10 @@
     margin-top: 8rem;
     width: 400px;
   }
-  select {
-    color: #666;
-    margin: 0;
-    border: none;
-    height: 34px;
-    font-size: 16px;
-  }
 </style>
 
 <main>
   <HomeHeader />
   <HomeSubtitle {swapped} {pickedCurrency} />
-  <select name="currency" id="currency" on:change={dispatchChange}>
-    {#each currencyArray as currency}
-      <option value={currency.toUpperCase()}>{currency.toUpperCase()}</option>
-    {/each}
-  </select>
+  <Select on:changeCurrency />
 </main>
---
+++ src/components/Select.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const currencyArray = ["usd", "eur", "czk"];
+  const dispatch = createEventDispatcher();
+  const dispatchChange = e => {
+    dispatch("changeCurrency", {
+      selected: e.target.value
+    });
+  };
+</script>
+
+<style>
+  select {
+    color: #666;
+    margin: 0;
+    border: none;
+    height: 34px;
+    font-size: 16px;
+  }
+</style>
+
+<select name="currency" id="currency" on:change={dispatchChange}>
+  {#each currencyArray as currency}
+    <option value={currency.toUpperCase()}>{currency.toUpperCase()}</option>
+  {/each}
+</select>
