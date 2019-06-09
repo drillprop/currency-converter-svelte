4ae01548096d9af658d9c52e4a5b2f912c724764
drillprops <drylbartosz@gmail.com>
2019-06-09 12:36:17 +0200

    create input fields for amount of money you want to convert

---
+++ src/components/AmountOfCurrency.svelte
@@ -0,0 +1,13 @@
+<style>
+  input {
+    color: #666;
+    padding: 4px 10px;
+    margin: 0;
+    font-size: 16px;
+    border: none;
+    width: 50px;
+    height: 25px;
+  }
+</style>
+
+<input type="text" />
