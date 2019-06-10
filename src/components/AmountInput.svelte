afb499546580a0e05bfbc2c119b7e8b710225b42
drillprops <drylbartosz@gmail.com>
2019-06-10 18:48:55 +0200

    handle amount input

--- src/components/AmountInput.svelte
+++ src/components/AmountInput.svelte
@@ -1,3 +1,16 @@
+<script>
+  import { amount } from "./stores";
+  import { tick } from "svelte";
+
+  const changeAmount = async e => {
+    amount.set(e.target.value);
+    await tick();
+    if (!e.target.value) {
+      amount.set(0);
+    }
+  };
+</script>
+
 <style>
   input {
     color: #666;
@@ -8,6 +21,16 @@
     width: 50px;
     height: 25px;
   }
+  /* remove arrows from input */
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 </style>
 
-<input type="text" />
+<input
+  type="number"
+  placeholder="0"
+  bind:value={$amount}
+  on:change={changeAmount} />
--- src/components/stores.js
+++ src/components/stores.js
@@ -1,3 +1,4 @@
 import { writable } from 'svelte/store';
 
 export const currency = writable('usd');
+export const amount = writable(0);
