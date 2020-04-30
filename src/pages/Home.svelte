3c36ab2602adb01dde14e379b6401fe771cfa136
drillprops <drylbartosz@gmail.com>
2020-04-30 21:52:42 +0200

    move everything from Main component to Home page and remove Main

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,6 +1,13 @@
 <script>
   import Aside from "../components/Aside.svelte";
+  import MainTitle from "../components/MainTitle.svelte";
+  import SubTitle from "../components/SubTitle.svelte";
   import Main from "../components/Main.svelte";
+  import AmountInput from "../components/AmountInput.svelte";
+  import SelectCurrency from "../components/SelectCurrency.svelte";
+  import ConvertedAmount from "../components/ConvertedAmount.svelte";
+  import SwapCurrency from "../components/SwapCurrency.svelte";
+  import { currency } from "../components/stores.js";
 
   let swapped = false;
 
@@ -9,5 +16,35 @@
   };
 </script>
 
+<style>
+  main {
+    margin: 0 auto;
+    margin-top: 8rem;
+  }
+</style>
+
 <!-- <Aside /> -->
-<Main {swapped} on:swapCurrency={swapCurrency} />
+<main>
+  <MainTitle title="Currency Converter" />
+  {#if swapped}
+    <SubTitle title={`Convert PLN to ${$currency}`} />
+  {:else}
+    <SubTitle title={`Convert ${$currency} to PLN`} />
+  {/if}
+
+  <p>
+    {#if !swapped}
+      <AmountInput />
+      <SelectCurrency />
+      equals
+      <ConvertedAmount {swapped} />
+      PLN
+    {:else}
+      <AmountInput />
+      PLN equals
+      <ConvertedAmount {swapped} />
+      <SelectCurrency />
+    {/if}
+  </p>
+  <SwapCurrency on:swapCurrency={swapCurrency} />
+</main>
