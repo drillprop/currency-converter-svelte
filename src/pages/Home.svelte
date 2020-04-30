0629a0badd758b145ce5602543daaef0c9053819
drillprops <drylbartosz@gmail.com>
2020-04-30 18:26:19 +0200

    rename HomeContainer to Home and move to pages

---
+++ src/pages/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Aside from "../components/Aside.svelte";
+  import Main from "../components/Main.svelte";
+
+  let swapped = false;
+
+  const swapCurrency = e => {
+    swapped = !swapped;
+  };
+</script>
+
+<!-- <Aside /> -->
+<Main {swapped} on:swapCurrency={swapCurrency} />
