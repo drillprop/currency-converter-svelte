facc378be90f7a6ead5e9e358eef7cf89d5328a0
drillprops <drylbartosz@gmail.com>
2020-04-30 19:36:02 +0200

    create MainTitle and SubTitle component

---
+++ src/components/MainTitle.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title;
+</script>
+
+<style>
+  h1 {
+    font-family: "Ubuntu", sans-serif;
+    color: #444;
+    margin: 0;
+    margin-bottom: 15px;
+    font-size: 40px;
+    font-weight: 800;
+  }
+</style>
+
+<h1>{title || ''}</h1>
---
+++ src/components/SubTitle.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { currency } from "./stores.js";
+  export let title;
+</script>
+
+<style>
+  h3 {
+    margin: 0;
+    margin-bottom: 20px;
+    font-size: 18px;
+    font-weight: 400;
+    color: #555;
+  }
+</style>
+
+<h3>{title || ''}</h3>
