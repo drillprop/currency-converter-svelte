fc55fa3520a2ceb3f9a521b90bbd756c8cd02cac
drillprops <drylbartosz@gmail.com>
2020-05-17 17:10:29 +0200

    create Rates page

---
+++ src/pages/Rates.svelte
@@ -0,0 +1,32 @@
+<script>
+  import MainTitle from "../components/MainTitle.svelte";
+  import ConvertedAmount from "../components/ConvertedAmount.svelte";
+
+  const currencies = ["usd", "eur", "czk"];
+</script>
+
+<style>
+  main {
+    margin: 0 auto;
+    margin-top: 8rem;
+  }
+</style>
+
+<main>
+  <MainTitle title="Current Exchange Rates" />
+
+  <p>Based on NBP API</p>
+  <table>
+    <tbody>
+
+      {#each currencies as currency (currency)}
+        <tr>
+          <td>{currency}</td>
+          <td>
+            <ConvertedAmount amount={1} {currency} swapped={false} />
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</main>
