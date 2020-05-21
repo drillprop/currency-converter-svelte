38028ea56098e9764efda9687a64a108bfbc334e
drillprops <drylbartosz@gmail.com>
2020-05-21 18:35:43 +0200

    replace AmountInputs with inputs tags

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,17 +1,31 @@
 <script>
   import MainTitle from "../components/MainTitle.svelte";
   import SubTitle from "../components/SubTitle.svelte";
-  import AmountInput from "../components/AmountInput.svelte";
   import SelectCurrency from "../components/SelectCurrency.svelte";
   import ConvertedAmount from "../components/ConvertedAmount.svelte";
   import SwapCurrency from "../components/SwapCurrency.svelte";
-  import { currency, amount } from "../components/stores.js";
+  import { currency } from "../components/stores.js";
 
   let swapped = false;
+  let amount = 0;
 
   const swapCurrency = e => {
     swapped = !swapped;
   };
+
+  const changeAmount = e => {
+    const { value, style } = e.target;
+    const { length } = value.toString();
+    amount = value;
+    if (length >= 4) {
+      style.width = 20 + value.length * 10 + "px";
+    }
+    if (!value) {
+      amount = 0;
+    }
+  };
+
+  $: console.log(amount);
 </script>
 
 <style>
@@ -19,29 +33,53 @@
     margin: 0 auto;
     margin-top: 8rem;
   }
+  input {
+    color: #666;
+    padding: 4px 10px;
+    margin: 0;
+    font-size: 16px;
+    border: none;
+    width: 50px;
+    text-indent: 5px;
+    height: 40px;
+  }
+  /* remove arrows from input */
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 </style>
 
 <main>
   <MainTitle title="Currency Converter" />
   {#if swapped}
-    <SubTitle title={`Convert PLN to ${$currency}`} />
+    <SubTitle title={`Convert PLN to ${$currency.toString().toUpperCase()}`} />
   {:else}
-    <SubTitle title={`Convert ${$currency} to PLN`} />
+    <SubTitle title={`Convert ${$currency.toString().toUpperCase()} to PLN`} />
   {/if}
 
   <p>
     {#if !swapped}
-      <AmountInput />
+      <input
+        type="number"
+        bind:value={amount}
+        on:input={changeAmount}
+        min={0} />
       <SelectCurrency />
       equals
-      <ConvertedAmount {swapped} amount={$amount} currency={$currency} />
+      <ConvertedAmount {swapped} {amount} currency={$currency} />
       PLN
     {:else}
-      <AmountInput />
+      <input
+        type="number"
+        bind:value={amount}
+        on:input={changeAmount}
+        min={0} />
       PLN equals
-      <ConvertedAmount {swapped} amount={$amount} currency={$currency} />
+      <ConvertedAmount {swapped} {amount} currency={$currency} />
       <SelectCurrency />
     {/if}
   </p>
-  <SwapCurrency on:swapCurrency={swapCurrency} amount={$amount} />
+  <SwapCurrency on:swapCurrency={swapCurrency} />
 </main>
